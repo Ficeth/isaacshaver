abc219af8277daf13fd47dce34295750da9d916e
Ficeth <154705345+Ficeth@users.noreply.github.com>
2026-03-10 18:06:14 +0100

    Added Button

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function pressButton() {
+  var x = document.getElementById("resume");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
