b744bda0e4b83e6d00a00a8cf38ad08f87100d52
Ficeth <154705345+Ficeth@users.noreply.github.com>
2026-03-10 19:24:29 +0100

    Whoops

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 function buttonPressed() {
   var x = document.getElementById("resume");
-  console.log("GRAAAAAH")
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
